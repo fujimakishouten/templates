8d75c69695bc5bb6452ec6af131ee1f8680279b0
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2016-08-31 10:28:09 +0900

    Add editorconfig, babel, eslint and module.exports template

---
+++ Templates/Default JavaScript module exports.js
@@ -0,0 +1,35 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/* eslint-env es6, mocha, node */
+/* eslint-extends: eslint:recommended */
+'use strict';
+
+
+
+// Variables
+const fs = require('fs'),
+    path = require('path');
+
+
+// Export modules
+fs.readdirSync(__dirname).forEach(function (filename) {
+    if ('index.js' === filename) {
+        return;
+    }
+
+    var extension = path.extname(filename),
+        name = filename.replace(extension, '');
+
+    module.exports[name] = require(path.join(__dirname, name));
+});
+
+
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
