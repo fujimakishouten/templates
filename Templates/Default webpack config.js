6f07f5ff739a3e3a3573d8ba002221171d0bc9ac
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2021-10-05 19:20:55 +0900

    Update webpack settings

--- Templates/Default webpack config.js
+++ Templates/Default webpack config.js
@@ -8,7 +8,8 @@
 // Variables
 const os = require('os'),
     path = require('path'),
-    webpack = require('webpack');
+    webpack = require('webpack'),
+    MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
 // Export module
@@ -16,65 +17,64 @@ module.exports = function (env) {
     const environment = env || {},
         parameters = {
             entry: {
-                app: './src/app.js'
+                app: './src/app.ts',
+                style: './styles/style.styl'
             },
             output: {
                 path : path.resolve(__dirname, './dest'),
                 filename: '[name].js'
             },
             module: {
-                rules: [
-                    /*{
-                        test: /\.js$/,
-                        enforce: 'pre',
-                        use: {
-                            loader: 'source-map-loader'
+                rules: [{
+                    test: /\.ts(x?)$/,
+                    exclude: /(node_modules|dest)/,
+                    use: [{
+                        loader: 'babel-loader',
+                        options: {
+                            comments: false,
+                            plugins: ['@babel/plugin-transform-runtime'],
+                            presets: [
+                                ['@babel/preset-env', {
+                                    useBuiltIns: 'usage',
+                                    corejs: 3,
+                                    targets: {
+                                        // node: 'current',
+                                        browsers: ['last 1 version']
+                                    }
+                                }]
+                            ]
                         }
-                    },
-                    {
-                    test: /\.tsx?$/,
-                    use: {
-                        loader: 'awesome-typescript-loader'
-                    }
-                    },*/
-                    {
-                        test: /\.js$/,
-                        exclude: /(node_modules|bower_components)/,
-                        use: {
-                            loader: 'babel-loader',
-                            options: {
-                                comments: false,
-                                plugins: ['@babel/plugin-transform-runtime'],
-                                presets: [
-                                    ["@babel/preset-env", {
-                                        "useBuiltIns": "usage",
-                                        "targets": {
-                                            // "node": "current",
-                                            "browsers": ["last 1 version"]
-                                        }
-                                    }]
-                                ]
-                            }
-                        }
-                    }
-                ]
+                    }, {
+                        loader: 'ts-loader'
+                    }]
+                }, {
+                    test: /\.styl$/,
+                    exclude: /(node_modules|dest)/,
+                    use: [{
+                        loader: MiniCssExtractPlugin.loader
+                    }, {
+                        loader: 'css-loader'
+                    }, {
+                        loader: 'stylus-loader'
+                    }]
+                }]
             },
             resolve: {
                 modules: [
                     path.resolve(__dirname, './src'),
-                    "node_modules"
+                    'node_modules'
                 ],
-                extensions: ["*", ".webpack.js", ".web.js", ".ts", ".tsx", ".js", ".json"]
+                extensions: ['*', '.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.json']
             },
             plugins: [
-                new webpack.ProvidePlugin({jQuery: "jquery"})
+                new MiniCssExtractPlugin({filename: 'style.css'})
             ],
-            target: "web",
+            target: 'web',
             performance: {
-                hints: "warning",
+                hints: 'warning',
                 maxAssetSize: 256 * 1024
             },
-            stats: "normal" // "detailed", "verbose"
+            stats: 'normal' // 'detailed', 'verbose'
         };
 
     if (environment.production) {
@@ -94,14 +94,4 @@ module.exports = function (env) {
 
     return parameters;
 };
-
-
-
-/*
- * Local variables:
- * tab-width: 4
- * c-basic-offset: 4
- * c-hanging-comment-ender-p: nil
- * End:
- */
 
